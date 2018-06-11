d19128a3d807688052550d56f3c086de0f79ac98
Jérémie Drouet <jeremie.drouet@gmail.com>
2018-06-11 16:32:11 +0200

    feat(pytorch): move script to install.sh

---
+++ opencv/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y cmake wget python3-pip
+
+pip3 install numpy
+
+wget -O /opencv.tar.gz https://github.com/opencv/opencv/archive/3.4.1-cvsdk.tar.gz
+wget -O /opencv-contrib.tar.gz https://github.com/opencv/opencv_contrib/archive/3.4.1.tar.gz
+
+tar xf opencv.tar.gz
+tar xf opencv-contrib.tar.gz
+
+mv opencv-3.4.1-cvsdk opencv
+mv opencv_contrib-3.4.1 opencv-contrib
+
+mkdir -p /opencv/build
+cd /opencv/build
+
+cmake \
+  -D CMAKE_BUILD_TYPE=RELEASE \
+  -D CMAKE_INSTALL_PREFIX=/usr/local \
+  -D INSTALL_PYTHON_EXAMPLES=ON \
+  -D OPENCV_EXTRA_MODULES_PATH=/opencv-contrib/modules \
+  -D BUILD_EXAMPLES=ON \
+  ..
+
+apt-get clean
+
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
---
+++ pytorch/install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+apt-get update
+
+apt-get install -y \
+  build-essential \
+  cmake \
+  cython \
+  g++ \
+  git \
+  libx11-6 \
+  libatlas-dev \
+  libblas-dev \
+  libopenblas-dev \
+  make \
+  python3 \
+  python3-dev \
+  python3-pip \
+  python3-yaml \
+
+python3 -m pip install numpy typing
+
+git clone --recursive https://github.com/pytorch/pytorch /pytorch
+
+cd /pytorch
+
+python3 setup.py build
+python3 setup.py install
+
+cd /
+
+apt-get remove -y \
+  build-essential \
+  g++ \
+  git \
+  make \
+  python3-dev
+
+apt-get clean
+
+rm -rf /pytorch
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
